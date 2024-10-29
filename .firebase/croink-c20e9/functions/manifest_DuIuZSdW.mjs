2cd7ad4ab693d45b4a169ebbc5a1c79fbe9cb73e
Raul Muela Morey <raulmuelamorey@gmail.com>
2024-10-29 09:48:34 +0100

    update

--- .firebase/croink-c20e9/functions/manifest_DuIuZSdW.mjs
+++
@@ -1,7 +0,0 @@
-import '@astrojs/internal-helpers/path';
-import 'cookie';
-export { m as manifest } from './chunks/_@astrojs-manifest_Q9W8F5EC.mjs';
-import 'kleur/colors';
-import 'html-escaper';
-import 'clsx';
-import './chunks/astro/server_CEIZ5UDn.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 import node from '@astrojs/node';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
